b09cab330892aa30d341e21e817d68f2a8e215fd
Ausar Mundra <ausar@marine-snow.co>
2023-08-24 05:44:56 -0400

    added vercel analytics

--- src/App.js
+++ src/App.js
@@ -5,26 +5,27 @@ import TwitterTimeline from './Twitter';
 import { SoundcloudEmbeds, YoutubeEmbed } from './Embeds';
 import {Today, ArtistSpotlight} from './Today';
 import { useState } from 'react';
-
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   const [hide, setHide] = useState(false);
 
     return (
-      <div className="App">
-       <h1><a href="/index">rap is dead</a></h1>
-        <button className="hideButton" onClick={()=>{setHide(!hide)}}>show|hide</button>
-        <img id="pic" width="350px" src={require('./dilla.jpg')} alt="j dilla"/>
-        <div id="shell" style={{display: hide ? "none" : "flex"}}>
-            <YoutubeEmbed/>
-            <SoundcloudEmbeds />
-            <Today />
-            <ArtistSpotlight />
-            <Discord />
-            <TwitterTimeline />
+        <div className="App">
+          <h1><a href="/index">rap is dead</a></h1>
+            <button className="hideButton" onClick={()=>{setHide(!hide)}}>show|hide</button>
+            <img id="pic" width="350px" src={require('./dilla.jpg')} alt="j dilla"/>
+            <div id="shell" style={{display: hide ? "none" : "flex"}}>
+                <YoutubeEmbed/>
+                <SoundcloudEmbeds />
+                <Today />
+                <ArtistSpotlight />
+                <Discord />
+                <TwitterTimeline />
+            </div>
+          <RapListBackground />
+          <Analytics />
         </div>
-      <RapListBackground />
-      </div>
     );
 };
 
